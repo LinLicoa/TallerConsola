850a6664fc9ca942ff340a5bf45c101ddef33e68
LinLicoa <villegas.licoa@gmail.com>
2023-12-04 22:04:39 -0500

    First Commit

---
+++ TallerConsola74/Program.cs
@@ -0,0 +1,117 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using TallerConsola74.Models;
+using TallerConsola74.Repositories;
+
+
+
+var optionsBuilder = new DbContextOptionsBuilder<TallerContext>();
+optionsBuilder.UseSqlServer(@"Server=DESKTOP-PC-LIN\SQLEXPRESS;Database=TallerConsola74;Trusted_Connection=True;Encrypt=False;");
+
+var tallerContext = new TallerContext(optionsBuilder.Options);
+
+try
+{
+    //tallerContext.Database.Migrate(); 
+    EstudianteRepository estudianteRepository = new EstudianteRepository(tallerContext);
+
+    Console.WriteLine("Ingrese los datos del estudiante");
+
+    Console.Write("Cédula: ");
+    string cedula = Console.ReadLine();
+
+    Console.Write("Nombre: ");
+    string nombre = Console.ReadLine();
+
+    Console.Write("Apellido: ");
+    string apellido = Console.ReadLine();
+
+    Console.Write("Curso ID: ");
+    int cursoId;
+    while (!int.TryParse(Console.ReadLine(), out cursoId))
+    {
+        Console.Write("Por favor ingrese un número válido para el Curso ID: ");
+    }
+
+    var estudiante = new Estudiante()
+    {
+        Cedula = cedula,
+        Nombre = nombre,
+        Apellido = apellido,
+        CursoId = cursoId
+    };
+
+    estudianteRepository.AgregarEstudiante(estudiante);
+    Console.WriteLine($"Estudiante {estudiante.Nombre} {estudiante.Apellido} agregado con id {estudiante.EstudianteId}");
+
+    //consultar estudiante por id   
+    Console.WriteLine("Ingrese el id del estudiante a consultar");
+    int id;
+    while (!int.TryParse(Console.ReadLine(), out id))
+    {
+        Console.Write("Por favor ingrese un número válido para el ID: ");
+    }
+    var estudianteConsultado = estudianteRepository.ConsultarEstudiantePorId(id);
+    Console.WriteLine($"Estudiante consultado {estudianteConsultado.Nombre} {estudianteConsultado.Apellido}");
+
+    //consultar todos los estudiantes
+    var estudiantes = estudianteRepository.ConsultarEstudiantes();
+
+    Console.WriteLine("Estudiantes registrados:\n");
+    for (int i = 0; i < estudiantes.Count; i++)
+    {
+        Console.WriteLine($"Estudiante {estudiantes[i].Nombre} {estudiantes[i].Apellido}"); 
+    }
+
+    int totalEstudiante = estudianteRepository.ContarEstudiantes();
+    Console.WriteLine("Total de estudiantes: " + totalEstudiante.ToString());
+
+    var primerEstudiante = estudianteRepository.ObtenerPrimerEstudiante();
+    Console.WriteLine("Primer elemento de la tabla: " + primerEstudiante.Cedula + "-" + primerEstudiante.Nombre);
+
+    // Consulta que devuelve a los estudiantes con id mayor que 2 y que el nombre sea igual a Anita
+    Console.WriteLine("Consulta que devuelve a los estudiantes con id mayor que 2 y que el nombre sea igual a Anita\r\n");
+    var anita = estudianteRepository.TraerAnita();
+    foreach (var item in anita)
+    {
+        Console.WriteLine("Codigo: " + item.EstudianteId + " Nombre: " + item.Nombre);
+    }
+
+    // Consulta que devuelve a los estudiantes donde el nombre sea igual Patty o Anita
+    Console.WriteLine("Consulta que devuelve a los estudiantes donde el nombre sea igual Patty o Anita");
+    var alumnas = estudianteRepository.TraerPattyAnita();
+    foreach (var item in alumnas)
+    {
+        Console.WriteLine("Codigo: " + item.EstudianteId + " Nombre: " + item.Nombre);
+    }
+
+    // Consulta que devuelve a los estudiantes que su nombre comienzan con la letra A
+    Console.WriteLine("Consulta que devuelve a los estudiantes que su nombre comienzan con la letra A");
+    var A = estudianteRepository.EstudiantesA();
+    foreach (var item in A)
+    {
+        Console.WriteLine("Codigo: " + item.EstudianteId + " Nombre: " + item.Nombre);
+    }
+
+    //var grupoEstudiante = estudianteRepository.GrupoEstudiantes;
+
+    //foreach (var resultado in grupoEstudiante)
+    //{
+    //    Console.WriteLine($"Nombre: {resultado.Nombre}, Cantidad: {resultado.Cantidad}");
+    //}
+
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+finally
+{
+    if (tallerContext != null)
+    {
+        tallerContext.Dispose();
+    }
+}
---
+++ TallerConsola74/Repositories/EstudianteRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TallerConsola74.Migrations;
+using TallerConsola74.Models;
+
+namespace TallerConsola74.Repositories
+{
+    public class EstudianteRepository
+    {
+        private readonly TallerContext _context;
+
+        public EstudianteRepository(TallerContext context)
+        {
+            _context = context;
+        }
+
+        public void AgregarEstudiante(Estudiante estudiante)
+        {
+            _context.Estudiantes.Add(estudiante);
+            _context.SaveChanges();
+        }   
+
+        public Estudiante ConsultarEstudiantePorId(int id)
+        {
+            return _context.Estudiantes.Find(id);
+        }
+
+        public List<Estudiante> ConsultarEstudiantes()
+        {
+            return _context.Estudiantes.ToList();
+        }
+
+        public Estudiante ObtenerPrimerEstudiante()
+        {
+            return _context.Estudiantes.First();
+        }
+
+        public int ContarEstudiantes()
+        {
+            return _context.Estudiantes.Count();
+        }
+
+        public List<Estudiante> TraerAnita()
+        {
+            // Consulta que devuelve a los estudiantes con id mayor que 2 y que el nombre sea igual a Anita
+            return _context.Estudiantes.Where(e => e.EstudianteId > 2 && e.Nombre == "Anita").ToList();
+        }
+
+        public List<Estudiante> TraerPattyAnita()
+        {
+            // Consulta que devuelve a los estudiantes donde el nombre sea igual Patty o Anita
+            return _context.Estudiantes.Where(e => e.Nombre == "Anita" || e.Nombre == "Patty").ToList();
+        }
+
+
+        public List<Estudiante> EstudiantesA()
+        {
+            // Consulta que devuelve a los estudiantes que su nombre comienzan con la letra A
+            return _context.Estudiantes.Where(e => e.Nombre.StartsWith("A")).ToList();
+        }
+
+        public List<Estudiante> GrupoEstudiantes()
+        {
+            // Uso de GroupBy
+            return _context.Estudiantes
+                .GroupBy(e => e.Nombre)
+                .Select(grupo => new Estudiante
+                {
+                    Nombre = grupo.Key,
+                    Cantidad = grupo.Count()
+                }).ToList();
+        }
+
+    }
+}
